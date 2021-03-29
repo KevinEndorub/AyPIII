13e7d06d65b4e4127672dcf7d19a47785a509083
KevinEndorub <73716228+KevinEndorub@users.noreply.github.com>
2021-03-29 16:54:19 -0300

    Ej1

--- Ej1.c
+++ Ej1.c
@@ -1,1 +1,16 @@
 #include <stdio.h>
+
+int main(){
+    int edad ;
+    char nombre[100]; 
+
+    printf ("Ingrese su nombre: ");
+    scanf("%s", nombre);
+    printf ("Ingrese su edad: ");
+    scanf("%d", &edad); //si es un numero pongo &
+    
+    printf("Su nombre es: %s \n",nombre);
+    printf("Su edad es: %d", edad);
+
+    return 0;
+};
